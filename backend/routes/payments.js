0ad7e0bfd515fdf80986281593997d75f1b49322
omar zouglah <114086583+zougmar@users.noreply.github.com>
2026-01-06 23:56:12 +0100

    c

---
+++ backend/routes/payments.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+// Payment routes - Coming soon
+// Payment functionality will be implemented in the future
+
+module.exports = router;
+
